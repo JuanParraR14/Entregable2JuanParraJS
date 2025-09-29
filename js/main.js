79ca8c4d60ac7993369c4ed932a99454798e0bff
JuanParra14 <juanparrar14@gmail.com>
2025-09-29 10:25:00 -0500

    Se añaden los elementos del HTLM a JS y se empieza a crear la lista de productos

---
+++ js/main.js
@@ -0,0 +1,58 @@
+const btnCarrito = document.getElementById("btn-carrito");
+const spanMostrarEsconderCarrito = document.getElementById("mostrar-esconder-carrito");
+const carrito = document.getElementById("carrito");
+const contenedorProductos = document.getElementById("contenedor-productos");
+const subtotal = document.getElementById("subtotal");
+const impuestos = document.getElementById("impuestos");
+const total = document.getElementById("total");
+const totalItems = document.getElementById("total-items");
+const limpiarCarrito = document.getElementById("limpiar-carrito");
+const contenedorProducto = document.getElementById("contenedor-producto");
+
+const productos = [
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+   {
+
+   },
+]
